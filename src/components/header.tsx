379b007e363492309e067f9b1678e1acb3ab5b5f
Eduardo <eduardo161205@gmail.com>
2025-08-25 14:56:43 -0300

    adição do componente de header sem funcionalidade

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
+
 export function Header() {
   return (
     <header className="flex justify-around items-center py-6">
       <div className="flex items-center justify-center max-w-3/4 w-full bg-white shadow-lg rounded-md p-6 gap-[3%]">
-        <img src="#" alt="logo astem" />
+        <Image
+          src="/images/logo_header.jpg"
+          alt="logo astem"
+          width={72}
+          height={72}
+        />
         <input
           type="text"
           className="min-w-1/3 bg-gray rounded-sm p-3 text-lg flex-1"
