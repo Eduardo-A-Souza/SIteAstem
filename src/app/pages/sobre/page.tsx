a448543bec28bc1204ce4035f9586db8c9531334
Eduardo <eduardo161205@gmail.com>
2025-08-26 17:57:50 -0300

    fix: correção das tags que fazem os links entre as paginas

---
+++ src/app/pages/sobre/page.tsx
@@ -0,0 +1,5 @@
+export default function sobre() {
+    return (
+        <h1>teste</h1>
+    )
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,35 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export function Header() {
   return (
     <header className="flex justify-around items-center py-6">
       <div className="flex items-center justify-center max-w-5/6 w-full bg-white shadow-lg rounded-md p-6 gap-[3%]">
+      <Link href="/">
         <Image
           src="/images/logo_header.jpg"
           alt="logo astem"
           width={72}
           height={72}
         />
+      </Link>
         <input
           type="text"
           className="min-w-1/3 bg-gray rounded-sm p-3 text-lg flex-1"
           placeholder="busque um produto"
         />
-        <a href="" className="text-xl">
+        <Link href="/" className="text-xl">
           Inicio
-        </a>
-        <a href="" className="text-xl">
+        </Link>
+        <Link href="/pages/produtos" className="text-xl">
           Produtos
-        </a>
-        <a href="" className="text-xl">
+        </Link>
+        <Link href="/pages/contato" className="text-xl">
           Contato
-        </a>
-        <a href="" className="text-xl">
+        </Link>
+        <Link href="/pages/sobre" className="text-xl">
           Sobre
-        </a>
+        </Link>
       </div>
     </header>
   );
