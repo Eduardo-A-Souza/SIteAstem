3084c0fad4c97611ae7980cfef0fd9e69ce67680
Eduardo <eduardo161205@gmail.com>
2025-08-29 15:59:45 -0300

    feat: adição da pagina de proteção de dados, adição da propriedade de title no componente sidebox style: remoção das bordas dos botões não selecionados

--- src/app/pages/missao-visao-valores/page.tsx
+++ src/app/pages/missao-visao-valores/page.tsx
@@ -4,7 +4,7 @@ import { FaBullseye, FaEye, FaHandshake } from "react-icons/fa";
 
 export default function missaoVisaoValores() {
   return (
-    <div className="flex justify-around items-center mx-30 gap-24 min-h-[65vh] m">
+    <div className="flex justify-around items-center mx-30 gap-24 min-h-[65vh]">
       <div className="flex flex-col items-center justify-around gap-10">
         <Image
           src="/images/logo_main.png"
@@ -42,6 +42,7 @@ export default function missaoVisaoValores() {
             label: "Missão, visão e valores",
             className: "",
           },
+          { href: "/pages/politica-privacidade", label: "Proteção de dados", className: ""}
         ]}
       ></SideBox>
     </div>
---
+++ src/app/pages/politica-privacidade/page.tsx
@@ -0,0 +1,80 @@
+import Image from "next/image";
+import { SideBox } from "@/components/SideBox";
+
+export default function politicaPrivacidade() {
+  return (
+    <div className="flex justify-around items-center mx-30 gap-24 min-h-[65vh] pb-10">
+      <div className="flex flex-col items-center gap-8">
+        <Image
+          src="/images/logo_main.png"
+          alt="logo astem"
+          width={390}
+          height={260}
+          className="mb-6"
+        />
+        <div className="flex flex-col gap-3">
+          <h1 className="text-6xl">Proteção de dados:</h1>
+          <p className="text-lg">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda
+            in est illum beatae earum unde, eaque sequi labore! Cupiditate
+            facere
+          </p>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h2 className="text-4xl">
+            I. Como são coletaodos os dados do usuário:
+          </h2>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+            optio dignissimos facere nihil veniam illo velit id commodi soluta
+            molestias fuga alias similique tempore quisquam ad sit, earum
+            excepturi. Modi!
+          </p>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h2 className="text-4xl">
+            II. Como são tratados os dados do usuário:
+          </h2>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+            optio dignissimos facere nihil veniam illo velit id commodi soluta
+            molestias fuga alias similique tempore quisquam ad sit, earum
+            excepturi. Modi!
+          </p>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h2 className="text-4xl">
+            III. Em quais situações os dados do usuário podem ser
+            compartilhados:
+          </h2>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+            optio dignissimos facere nihil veniam illo velit id commodi soluta
+            molestias fuga alias similique tempore quisquam ad sit, earum
+            excepturi. Modi!
+          </p>
+        </div>
+      </div>
+      <SideBox
+        title="Navegação rapida:"
+        links={[
+          {
+            href: "#",
+            label: "I.Como são coletados os dados...",
+            className: "",
+          },
+          {
+            href: "#",
+            label: "II. como são tratados os dados...",
+            className: "",
+          },
+          {
+            href: "#",
+            label: "III. Em quais situações os dados ...",
+            className: "",
+          },
+        ]}
+      />
+    </div>
+  );
+}
